de5ccc4b39d07044d08d9861481d952459c79d13
Manish Verma <manishverma.24@gmail.com>
2019-08-19 16:09:13 -0400

    2nd commit

--- manish_verma_interview.py
+++ manish_verma_interview.py
@@ -9,6 +9,7 @@ assert "Validus - Careers" in driver.title
 elem = driver.find_element_by_class_name("//a[@class='btn btn-primary']")
 elem.click()
 
+# there might be some issue since this opens in new tab, so we need to handle that case.
 time.sleep(2)
 
 driver.find_element_by_xpath("//div[@id='wd-FacetValue-CheckBox-bc33aa3152ec42d4995f4791a106ed09']//div[@class='WHGF']").click()
@@ -19,7 +20,12 @@ results= get_text("//span[@id='wd-FacetedSearchResultList-PaginationText-facetSe
 noofresults = results.split(" ")
 print("noofresults[0]")
 
-
+elements = driver.find_elements_by_class("localtor for class ")
+for item in elements:
+	txt = item.get_text()
+	if "Today" in txt:
+		print ("find the job id here")
+		
 
 
 driver.find_element_by_xpath("//span[contains(text(),'Clear All')]").click()
